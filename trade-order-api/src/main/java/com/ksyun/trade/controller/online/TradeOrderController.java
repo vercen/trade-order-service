b9ff9dbc6e7cd063d07dd67d57136cfd54a9ba4f
fuxiaoming <fuxiaoming@kingsoft.com>
2023-07-24 23:08:59 +0800

    refactor : 代码重构，抽出公共部分

--- trade-order-api/src/main/java/com/ksyun/trade/controller/online/TradeOrderController.java
+++ trade-order-api/src/main/java/com/ksyun/trade/controller/online/TradeOrderController.java
@@ -1,43 +1,41 @@
 package com.ksyun.trade.controller.online;
 
 import com.ksyun.trade.rest.RestResult;
-import com.ksyun.trade.service.OrdertByConfig;
 import com.ksyun.trade.service.TradeOrderService;
-import com.ksyun.trade.service.UserService;
 import com.ksyun.trade.vo.OrderVo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @author Administrator
- */
 @RestController
-@RequestMapping(value = "/online/trade_order", produces = {MediaType.APPLICATION_JSON_VALUE})
+@RequestMapping("/online/trade_order")
 @Slf4j
 public class TradeOrderController {
+    private static final String ORDER_VO_KEY_PREFIX = "orderVo:";
+
     @Autowired
     private TradeOrderService orderService;
+
     @Autowired
-    private RedisTemplate<String, Object> redisTemplate;
+    private RedisTemplate<String ,Object> redisTemplate;
 
     @RequestMapping("/{id}")
     public RestResult query(@PathVariable("id") Integer id, @RequestHeader("upsteam") String upsteam) {
-        OrderVo orderVo = (OrderVo) redisTemplate.opsForValue().get("orderVo:" + id);
+        OrderVo orderVo =(OrderVo) redisTemplate.opsForValue().get(ORDER_VO_KEY_PREFIX + id);
         if (orderVo != null) {
             orderVo.setUpsteam(upsteam);
+            log.info("订单ID: {}，缓存命中", id);
             return RestResult.success().data(orderVo);
         }
+
         orderVo = orderService.query(id);
         orderVo.setUpsteam(upsteam);
-        redisTemplate.opsForValue().set("orderVo:" + id, orderVo);
+        redisTemplate.opsForValue().set(ORDER_VO_KEY_PREFIX + id, orderVo);
+        log.info("订单ID: {}，缓存未命中，添加到缓存", id);
         return RestResult.success().data(orderVo);
     }
-
 }
--- trade-order-api/src/main/java/com/ksyun/trade/service/TradeOrderService.java
+++ trade-order-api/src/main/java/com/ksyun/trade/service/TradeOrderService.java
@@ -3,7 +3,6 @@ package com.ksyun.trade.service;
 import com.ksyun.trade.mapper.OrderMapper;
 import com.ksyun.trade.pojo.KscTradeOrder;
 import com.ksyun.trade.pojo.KscTradeProductConfig;
-import com.ksyun.trade.pojo.Region;
 import com.ksyun.trade.pojo.User;
 import com.ksyun.trade.vo.OrderVo;
 import com.ksyun.trade.vo.RegionVo;
@@ -13,29 +12,35 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
-
+import java.util.concurrent.TimeUnit;
 
 @Service
 @Slf4j
 public class TradeOrderService {
-    @Autowired
-    private OrderMapper orderMapper;
-    @Autowired
-    UserService userService;
-    @Autowired
-    private OrdertByConfig ordertByConfig;
-    @Autowired
-    private RegionListService regionList ;
+    private static final String ORDER_CACHE_KEY_PREFIX = "order:";
+    private static final long ORDER_CACHE_EXPIRATION_SECONDS = 3600; // 缓存过期时间为1小时
+    private final OrderMapper orderMapper;
+    private final UserService userService;
+    private final OrdertByConfig ordertByConfig;
+    private final RegionListService regionList;
+    private final RedisTemplate<String, Object> redisTemplate;
 
     @Autowired
-    private RedisTemplate<String, Object> redisTemplate;
+    public TradeOrderService(OrderMapper orderMapper, UserService userService,
+                             OrdertByConfig ordertByConfig, RegionListService regionList,
+                             RedisTemplate<String, Object> redisTemplate) {
+        this.orderMapper = orderMapper;
+        this.userService = userService;
+        this.ordertByConfig = ordertByConfig;
+        this.regionList = regionList;
+        this.redisTemplate = redisTemplate;
+    }
 
     public OrderVo query(Integer id) {
-        KscTradeOrder tradeOrder = (KscTradeOrder) redisTemplate.opsForValue().get("order:" + id);
+        KscTradeOrder tradeOrder = (KscTradeOrder) redisTemplate.opsForValue().get(ORDER_CACHE_KEY_PREFIX + id);
         if (tradeOrder == null) {
             tradeOrder = orderMapper.queryById(id);
-            redisTemplate.opsForValue().set("order:" + id, tradeOrder);
+            redisTemplate.opsForValue().set(ORDER_CACHE_KEY_PREFIX + id, tradeOrder, ORDER_CACHE_EXPIRATION_SECONDS, TimeUnit.SECONDS);
         }
         //调用用户查询服务
         User user = userService.querybyid(tradeOrder.getUserId());
@@ -51,5 +56,4 @@ public class TradeOrderService {
         orderVo.setConfigs(orderconfig);
         return orderVo;
     }
-
 }
